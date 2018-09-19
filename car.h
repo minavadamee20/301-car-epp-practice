7c72e7520843835d7e95e23d702a6cd2189b7911
minavadamee20 <34077203+minavadamee20@users.noreply.github.com>
2018-09-18 18:03:21 -0700

    forming of ifstream

--- car.h
+++ car.h
@@ -2,15 +2,31 @@
 #define car_h
 #include <iostream>
 #include <string>
+#include <fstream>
+#inclde <vector>
 
 struct Cars{
   std::string modelName_;
   std::string license_;
   int mileage_;
+  std::vector<std::string> vModelName;
+  ifstream myfiles_;
 
   Cars:Cars("", "", 0){}
   Cars(const std::string& modelName, const std::string& license, int mileage): modelName_(modelName), license_(license), mileage_(mileage){}
-  
+
+  void obtainstuff(std::string filename){
+    myfiles_.open(filename);
+    if(!myfiles_.is_open()){
+      std::cerr << "Error in opening: " << filename <<  '\n';
+    }
+    else{
+
+    }
+    myfiles_.close();
+  }
+
+
 }
 
 #endif
